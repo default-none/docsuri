54b4199307537738f1100166dfe3475e67e2ee82
default-none <foxrain426@gmail.com>
2023-11-16 17:44:47 +0900

    layout0.0.1

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
+import { Route, Routes } from "react-router-dom";
+import { Page as MainPage } from "./pages/MainPage/Page";
 
 function App() {
   return (
-<>
-</>
+    <>
+      <Routes>
+        <Route path="/" element={<MainPage />} />
+      </Routes>
+    </>
   );
 }
 
